da78aa9e7b63928aac29c7a7a7aed726b3593245
Marbin Karaus <marbin@2do-digital.de>
2024-11-04 16:34:40 +0100

    🔧 Switched from mocha to macchiato

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.g.background = "light"
+vim.g.background = "dark"
 
 vim.opt.swapfile = false
 
@@ -14,5 +14,6 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.opt.scrolloff = 10
 vim.wo.number = true
 vim.o.relativenumber = true
